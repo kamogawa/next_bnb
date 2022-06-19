d56485afc1506d97e41977652f4105ac527aaf3e
BEUMGOO KIM <captain_kbg@naver.com>
2022-06-19 17:49:47 +0900

    project setting

---
+++ styles/GlobalStyle.tsx
@@ -0,0 +1,21 @@
+import reset from "styled-reset";
+import { createGlobalStyle, css } from "styled-components";
+import palette from "./palette";
+
+const globalStyle = css`
+  ${reset};
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: 'Roboto', sans-serif;
+    color: ${palette.black};
+  }
+`;
+
+const GlobalStyle = createGlobalStyle`
+    ${globalStyle};
+`;
+
+export default GlobalStyle;
